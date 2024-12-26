654c3db44d338eb93f410c5e975339fc3a4c4621
dnwn1211 <128039034+dnwn1211@users.noreply.github.com>
2024-12-26 10:43:25 +0900

    Blip Inside commit

---
+++ PoketBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PoketBook
+//
+//  Created by 김석준 on 12/26/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
